47b3b5d8c17a20a9197516838990c44a11fab89e
kuncoroatmojo <kuncoro.atmojo@gmail.com>
2014-03-09 00:03:09 +0700

    geo api

---
+++ backend/geographicapi.php
@@ -0,0 +1,12 @@
+<?php
+function getapikey(){
+	return "426c044849d8f98b1591c2643275eca3";
+}
+function getjsonarea($lat,$long){
+	$key
+	$ret = file_get_contents("/geographic/api/point?apiKey=$key&lat=$lat&long=$long");
+	return 
+}
+
+
+?>
