c9a19a39c71d3af2192f80acbe059ff33c1da2fd
kuncoroatmojo <kuncoro.atmojo@gmail.com>
2014-03-09 00:16:36 +0700

    api.pemilu.org

--- backend/geographicapi.php
+++ backend/geographicapi.php
@@ -4,13 +4,14 @@ function getapikey(){
 }
 function getjsonarea($lat,$long){
 	$key = getapikey();
-	$ret = file_get_contents("/geographic/api/point?apiKey=".$key."&lat=".$lat."&long=".$long);
+	$ret = file_get_contents("http://api.pemilu.org/geographic/api/point?apiKey=".$key."&lat=".$lat."&long=".$long);
 	return $ret;
 }
 
 function getareabyid($id){
+//return detail of an area
 	$key = getapikey();
-	$ret = file_get_contents("/geographic/api/area/".$id."?apiKey=".$key);
+	$ret = file_get_contents("http://api.pemilu.org/geographic/api/area/".$id."?apiKey=".$key);
 	return $ret;
 }
 
