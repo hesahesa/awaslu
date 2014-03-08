5e486a0b306cf7fe0eeb7e4fcf3807d1f5f12d48
hesahesa <prahesa@yahoo.com>
2014-03-09 05:35:29 +0700

    db

--- backend/awaslu.sql
+++ backend/awaslu.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 08, 2014 at 10:26 
+-- Generation Time: Mar 09, 2014 at 05:35 
 -- Server version: 5.1.41
 -- PHP Version: 5.3.1
 
@@ -36,19 +36,23 @@ CREATE TABLE IF NOT EXISTS `report_tbl` (
   `latitude` varchar(64) NOT NULL,
   `longitude` varchar(64) NOT NULL,
   `party_id_API` varchar(64) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `user_id` varchar(32) NOT NULL,
+  `area_id_API` varchar(64) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `report_tbl`
 --
 
-INSERT INTO `report_tbl` (`id`, `title`, `picture_url`, `description`, `date`, `caleg_id_API`, `latitude`, `longitude`, `party_id_API`, `user_id`) VALUES
-(1, 'laporan 1', '12345', 'ini deskripsi laporan 1', '2014-03-08 15:56:13', 'calegid-1', 'latitude', 'longitude', 'partyid-1', 1),
-(2, 'laporan 2', '1234', 'deskripsi laporan 2', '2014-03-08 16:36:09', 'calegid-2', 'latitude', 'longitude', 'partyid-1', 1),
-(3, 'laporan 3', '1234', 'deskripsi laporan 3', '2014-03-08 17:45:28', 'calegid-1', 'latitude', 'longitude', 'partyid-1', 1),
-(4, 'laporan 4', 'blaballa', 'deskripsi laporan 4', '2014-03-07 21:12:17', 'calegid-2', 'latitude', 'longitude', 'partyid-1', 3);
+INSERT INTO `report_tbl` (`id`, `title`, `picture_url`, `description`, `date`, `caleg_id_API`, `latitude`, `longitude`, `party_id_API`, `user_id`, `area_id_API`) VALUES
+(1, 'laporan 1', '12345', 'ini deskripsi laporan 1', '2014-03-08 15:56:13', 'calegid-1', 'latitude', 'longitude', 'partyid-1', '1', '32'),
+(2, 'laporan 2', '1234', 'deskripsi laporan 2', '2014-03-08 16:36:09', 'calegid-2', 'latitude', 'longitude', 'partyid-1', '1', '32'),
+(3, 'laporan 3', '1234', 'deskripsi laporan 3', '2014-03-08 17:45:28', 'calegid-1', 'latitude', 'longitude', 'partyid-1', '1', '75'),
+(4, 'laporan 4', 'blaballa', 'deskripsi laporan 4', '2014-03-07 21:12:17', 'calegid-2', 'latitude', 'longitude', 'partyid-1', '3', '75'),
+(5, 'aaa', 'aaa', 'aaa', '2014-03-09 04:46:52', 'aaa', 'aaa', 'aaa', 'aaa', '0', 'aaa'),
+(6, 'a', 'a', 'a', '2014-03-09 04:52:55', 'a', 'a', 'a', 'a', 'a', 'a'),
+(7, 'ads', '{"status":"success","file_name":"CE590134-EB29-4619-A42D-BA712063E495_DSC0131.jpg"}', 'ad', '2014-03-09 04:56:54', 'ad', 'sdf', 'sdf', 'saf', 'sdf', 'wf');
 
 -- --------------------------------------------------------
 
