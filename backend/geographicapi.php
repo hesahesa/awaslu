5484c9c07012e6e7be9923cf7e3c2e72733b3eb5
kuncoroatmojo <kuncoro.atmojo@gmail.com>
2014-03-09 00:08:35 +0700

    fixing

--- backend/geographicapi.php
+++ backend/geographicapi.php
@@ -3,10 +3,14 @@ function getapikey(){
 	return "426c044849d8f98b1591c2643275eca3";
 }
 function getjsonarea($lat,$long){
-	$key
-	$ret = file_get_contents("/geographic/api/point?apiKey=$key&lat=$lat&long=$long");
-	return 
+	$key = getapikey();
+	$ret = file_get_contents("/geographic/api/point?apiKey=".$key."&lat=".$lat."&long=".$long);
+	return $ret;
 }
 
+function getareabyid($id){
+	$key = getapikey();
+	$ret = file_get_contents("/geographic/api/area/".$id."?apiKey=".$key);
+}
 
 ?>
