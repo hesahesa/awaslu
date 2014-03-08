c97afe2c35353eb9f55f3c2d4f0b0306f9bb092b
Ardall <ardall.leonardo@gmail.com>
2014-03-08 18:02:01 +0700

    Added icon, updated js libary

--- javascript/NeoClientSource/models/controller.js
+++ javascript/NeoClientSource/models/controller.js
@@ -3,25 +3,33 @@
     this.reports = [];
     this.hasReport = false;
     this.totReport = 0;
+};
 
-    this.getReportList = function (_view) {
-        var ajaxCall = new pemilu.util.ajaxCall();
-        ajaxCall.getReportList(function (response) {
-            _this.setReportList(response, _view);
-            //force to re-bind
-            _view.bind();
-        });
-    }
-
-    this.setReportList = function (data, _view) {
-        //create random ukm list, later fetch it using ajax call
-        for (i = 0; i < 19 ; i++) {
-			var report_title = "sample_name" + i;
-            var jsonData = [{ "report_id": i, "report_title": report_title}];
-            console.log(jsonData[0]);
-            this.reports[i] = new pemilu.report(jsonData[0]);
-        }
+pemilu.controller.prototype.getReportList = function (_view) {
+	var ajaxCall = new pemilu.util.ajaxCall();
+	ajaxCall.getReportList(function (response) {
+		_this.setReportList(response, _view);
+		//force to re-bind
 		_view.bind();
-    }
+	});
+};
 
+pemilu.controller.prototype.setReportList = function (data, _view) {
+	//create random ukm list, later fetch it using ajax call
+	for (i = 0; i < 19 ; i++) {
+		var title = "sample_name" + i;
+		var jsonData = [{ "report_id": i, "title": title, "pic_url": "http://stat.ks.kidsklik.com/statics/files/2014/01/13887804611655356549.gif"}];
+		console.log(jsonData[0]);
+		this.reports[i] = new pemilu.report(jsonData[0]);
+	}
+	_view.bind();
+};
+
+pemilu.controller.prototype.getMostSharedReportList	= function (_view) {
+	var ajaxCall = new pemilu.util.ajaxCall();
+	ajaxCall.getMostSharedReportList(function (response) {
+		_this.setReportList(response, _view);
+		//force to re-bind
+		_view.bind();
+	});
 };
