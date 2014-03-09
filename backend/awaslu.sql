c593dd8fd39b5f929119eeca9949a82fe43c5ee6
kuncoroatmojo <kuncoro.atmojo@gmail.com>
2014-03-09 09:46:27 +0700

    sql

--- backend/awaslu.sql
+++ backend/awaslu.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 09, 2014 at 03:26 AM
+-- Generation Time: Mar 09, 2014 at 03:45 AM
 -- Server version: 5.5.8
 -- PHP Version: 5.3.5
 
@@ -45,10 +45,10 @@ CREATE TABLE IF NOT EXISTS `report_tbl` (
 --
 
 INSERT INTO `report_tbl` (`id`, `title`, `picture_url`, `description`, `date`, `caleg_id_API`, `latitude`, `longitude`, `party_id_API`, `user_id`, `area_id_API`) VALUES
-(1, 'laporan 1', '12345', 'ini deskripsi laporan 1', '2014-03-08 15:56:13', 'calegid-1', 'latitude', 'longitude', 'partyid-1', '1', '32'),
-(2, 'laporan 2', '1234', 'deskripsi laporan 2', '2014-03-08 16:36:09', 'calegid-2', 'latitude', 'longitude', 'partyid-1', '1', '32'),
-(3, 'laporan 3', '1234', 'deskripsi laporan 3', '2014-03-08 17:45:28', 'calegid-1', 'latitude', 'longitude', 'partyid-1', '1', '75'),
-(4, 'laporan 4', 'blaballa', 'deskripsi laporan 4', '2014-03-07 21:12:17', 'calegid-2', 'latitude', 'longitude', 'partyid-1', '3', '75'),
+(1, 'Mengganggu Ketertiban Umum', 'images.jpg', 'ini deskripsi laporan 1', '2014-03-08 15:56:13', '1100-00-0000-0001', 'latitude', 'longitude', 'partyid-1', '1', '11'),
+(2, 'Mengganggu Fasilitas Umum', 'fasilitasumum.jpg', 'deskripsi laporan 2', '2014-03-08 16:36:09', '1100-01-0000-0107', 'latitude', 'longitude', 'partyid-1', '1', '32'),
+(3, 'Poster merusak lingkungan', 'pelanggaran2.jpg', 'deskripsi laporan 3', '2014-03-08 17:45:28', '3200-00-0000-0004', 'latitude', 'longitude', 'partyid-1', '1', '75'),
+(4, 'Gambar Caleg di Rumah Ibadah', 'rumahibadah.jpg', 'deskripsi laporan 4', '2014-03-07 21:12:17', '3200-00-0000-0008', 'latitude', 'longitude', 'partyid-1', '3', '32'),
 (5, 'aaa', 'aaa', 'aaa', '2014-03-09 04:46:52', 'aaa', 'aaa', 'aaa', 'aaa', '0', 'aaa'),
 (6, 'a', 'a', 'a', '2014-03-09 04:52:55', 'a', 'a', 'a', 'a', 'a', 'a'),
 (7, 'ads', '{"status":"success","file_name":"CE590134-EB29-4619-A42D-BA712063E495_DSC0131.jpg"}', 'ad', '2014-03-09 04:56:54', 'ad', 'sdf', 'sdf', 'saf', 'sdf', 'wf'),
